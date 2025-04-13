689fd78b9ca22d5bd2d12d836048945b87fcc086
Zhanerke1900 <148362168+Zhanerke1900@users.noreply.github.com>
2025-04-14 04:50:30 +0500

    Добавлен index.html для GitHub Pages

--- script.js
+++ script.js
@@ -138,3 +138,11 @@ function showToast(toast) {
     }
   }
 
+  function toggleMenu() {
+    const menu = document.getElementById("side-menu");
+    if (menu.style.right === "0px") {
+      menu.style.right = "-250px";
+    } else {
+      menu.style.right = "0px";
+    }
+  }
--- script_uz.js
+++ script_uz.js
@@ -138,4 +138,11 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   
-  
+    function toggleMenu() {
+      const menu = document.getElementById("side-menu");
+      if (menu.style.right === "0px") {
+        menu.style.right = "-250px";
+      } else {
+        menu.style.right = "0px";
+      }
+    }
